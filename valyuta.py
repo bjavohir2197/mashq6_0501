94654caa31dae806d86d197585fee82347878ee9
bjavohir2197 <bjavohir2197@gmail.com>
2026-01-05 13:01:07 +0500

    Create valyuta.py

---
+++ valyuta.py
@@ -0,0 +1,12 @@
+USD = 12_700
+EUR = 13500
+RUB = 140
+
+def usd_uzs(miqdor):
+    return miqdor * USD
+
+def eur_uzs(miqdor):
+    return miqdor * EUR
+
+def rub_uzs(miqdor):
+    return miqdor * RUB
